f2d05fe045d1175f37309f9a11f7e7c5a80a2c5d
Ravi Mashru <mashru.ravi@gmail.com>
2020-03-23 07:10:58 +0530

    Add terraform script

---
+++ terraform/prod.tf
@@ -0,0 +1,68 @@
+variable "region" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+  shared_credentials_file = "./credentials"
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound."
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" = "true"
+  }
+
+}
+
+resource "aws_instance" "prod_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+region            = "ap-south-1"
+instance_type     = "t2.micro"
+ami_id            = "ami-0620d12a9cf777c87"
